8a2720c4762adef28ae26ebc53679ec897c2b906
program.DU <bernardinodaniela96@gmail.com>
2023-11-16 10:38:36 -0600

    02 Se agrega el calculo para la matriz superior e inferior

--- src/matriztriangular/MatrizTriangular.java
+++ src/matriztriangular/MatrizTriangular.java
@@ -7,22 +7,35 @@ public class MatrizTriangular {
     public static void main(String[] args) { 
         
         int dim;
-        int[][] matriz;
+        double[][] matriz;
+        double[][] superior;
+        double[][] inferior;
         
         System.out.println("Matriz triangular");
         System.out.println("Ingresa la dimension de la matriz: ");
         dim = en.nextInt();
         
+        superior=new double[dim][dim];
+        inferior=new double[dim][dim];
+        // Se llena la matriz
         matriz = llenar(dim);
+        // Se calcula tanto la matriz superior como la inferior de la matriz original
+        calcularM(matriz, inferior, superior);
         
+        // Imprimimos todas las matrices
+        System.out.println("Matriz original");
         imprimir(matriz);
-        
-        comprobar(matriz);
+        System.out.println("");
+        System.out.println("Matriz triangular superior");
+        imprimir(superior);
+        System.out.println("");
+        System.out.println("Matriz triangular inferior");
+        imprimir(inferior);
         
     }
     
-    public static int[][] llenar(int dim){
-        int[][] matriz = new int[dim][dim];
+    public static double[][] llenar(int dim){
+        double[][] matriz = new double[dim][dim];
         
         for(int i=0; i<dim; i++){
             for(int j=0; j<dim; j++){
@@ -34,7 +47,7 @@ public class MatrizTriangular {
         return matriz;
     }
     
-    public static void imprimir(int[][] matriz){
+    public static void imprimir(double[][] matriz){
         for(int i=0; i<matriz.length; i++){
             for(int j=0; j<matriz.length; j++){
                 System.out.print(matriz[i][j] + "   ");
@@ -43,46 +56,23 @@ public class MatrizTriangular {
         }
     }
     
-    public static void comprobar(int[][] matriz){
-        int superior=0, inferior=0;
-        int aux = 2;
-        //int k=matriz.length-1;
-        /* Superior */
-        for(int i=matriz.length-1; i>=1; i--){
-            
-            for(int j=matriz.length-aux; j>=0; j--){
-                if(matriz[i][j]==0){
-                    superior = 1;
-                }
-                else{
-                    superior = 0;
-                    break;
+    private static void calcularM(double[][] original, double[][] inferior, double[][] superior) {
+
+        for (int k = 0; k < original.length; k++) {
+            inferior[k][k] = 1; // Diagonal principal de la matriz inferior es 1
+
+            for (int i = k + 1; i < original.length; i++) {
+                double factor = original[i][k] / original[k][k];
+                inferior[i][k] = factor;
+
+                for (int j = k; j < original.length; j++) {
+                    original[i][j] -= factor * original[k][j];
                 }
             }
-            if(superior==0){
-                break;
+
+            for (int j = k; j < original.length; j++) {
+                superior[k][j] = original[k][j];
             }
-            aux++;
         }
-        /* Inferior */
-        /*for(int i=matriz.length-1; i>=1; i--){
-            for(int j=matriz.length-2; j>=0; j--){
-                if(matriz[i][j]==0){
-                    inferior = 1;
-                }
-                else{
-                    superior = 0;
-                }
-            }
-        }*/
-        if(superior == 1){
-            System.out.println("Es una matriz triangular superior");
-        }
-        /*if(inferior == 1){
-            System.out.println("Es una matriz triangular inferior");
-        }
-        if(superior == 1 && inferior == 1){
-            System.out.println("Ni inferior ni superior");
-        }*/
     }
 }
